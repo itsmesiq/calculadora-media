c02a482272ecd39c43f1006b890cacb0569de522
Ana Siqueira <anasiqueira.serpentis@gmail.com>
2025-07-28 22:45:13 -0300

    updated script and styles

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,37 @@
+document.getElementById("calcular").addEventListener("click", calcularMedia);
+
 function calcularMedia() {
-    const nota1 = parseFloat(document.getElementById("nota1").value);
-    const nota2 = parseFloat(document.getElementById("nota2").value);
-    const corte = parseFloat(document.getElementById("corte").value);
+    const inputNota1 = document.getElementById("nota1").value;
+    const inputNota2 = document.getElementById("nota2").value;
+    const inputCorte = document.getElementById("corte").value;
+    const resultado = document.getElementById("resultado");
+
+    resultado.className = "";
+
+    if(inputNota1 === "" || inputNota2 === "" || inputCorte === ""){
+        resultado.textContent = "Por favor, preencha todas as notas";
+        resultado.classList.add("resultado-erro");
+        return;
+    }
+    
+
+    const nota1 = parseFloat(inputNota1);
+    const nota2 = parseFloat(inputNota2);
+    const corte = parseFloat(inputCorte);
+    
+    if(isNaN(nota1) || isNaN(nota2) || isNaN(corte)){
+        resultado.textContent = "Por favor, insira números válidos"
+        resultado.classList.add("resultado-erro");
+        return;
+    }
 
     const media = (nota1 + nota2) / 2;
 
     if (media >= corte){
-        result.textContent = 'Sua média é ${media}. Parabéns, você foi aprovado! 🎉';
-        result.style.color = "green";
+        resultado.textContent = `Sua média é ${media}. Parabéns, você foi aprovado! 🎉`;
+        resultado.classList.add("resultado-aprovado");
     } else {
-        result.textContent = "Sua média é ${media}. Infelizmente, você foi reprovado. 😢";
-        result.style.color = "red";
+        resultado.textContent = `Sua média é ${media}. Infelizmente, você foi reprovado. 😢`;
+        resultado.classList.add("resultado-reprovado");
     }
 }
